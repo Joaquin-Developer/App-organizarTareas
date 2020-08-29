9dddaba163c28c9015ef89ce0611b441064ae5fc
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-29 17:49:05 -0300

    actualizo objetos de fechas en clase tareas

--- organizarTareas/src/logica/PruebaObjetos.java
+++ organizarTareas/src/logica/PruebaObjetos.java
@@ -1,9 +1,12 @@
 package logica;
 
-//import java.text.SimpleDateFormat;
+import java.text.SimpleDateFormat;
 //import java.text.ParseException;
-//import java.util.Date; 
+import java.util.Date; 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.time.LocalDate;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -11,14 +14,22 @@ import java.time.LocalDate;
  */
 public class PruebaObjetos {
     
+    private static LocalDate fecha;
+    
     public static void main(String[] args) {
         //SimpleDateFormat objSDF = new SimpleDateFormat("dd-mm-yyyy"); 
         LocalDate ld = LocalDate.now();
-        System.out.println(ld);
+
         System.out.print(ld.getDayOfMonth() + "/");
         System.out.print(ld.getMonthValue() + "/");
         System.out.print(ld.getYear());
         // print: 26/8/2020 [ok]
+        
+        fecha = LocalDate.now();
+        JOptionPane.showMessageDialog(null, fecha.toString(), "LocalDate", JOptionPane.PLAIN_MESSAGE);
+        
+        
+        
     }
     
 }
