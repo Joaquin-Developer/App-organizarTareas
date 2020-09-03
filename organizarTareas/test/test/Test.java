958cd061a200bf70e718206cf76f1e1e7aa45436
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-09-03 18:44:09 -0300

    finalizo (por hoy) testeo de Tarea.java en Test.java

--- organizarTareas/test/test/Test.java
+++ organizarTareas/test/test/Test.java
@@ -1,31 +1,27 @@
 package test;
 
-import java.text.SimpleDateFormat;
+//import java.text.SimpleDateFormat;
 //import java.text.ParseException;
-import java.util.Date; 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+//import java.util.Date; 
+//import java.util.Calendar;
+//import java.util.GregorianCalendar;
 import java.time.LocalDate;
 import javax.swing.JOptionPane;
 import logica.*;
 
 public class Test {
     
-    private LocalDate fecha;
+    //private LocalDate fecha;
     
-    
-    public void prueba1() {
+    public static void prueba1() {
         //SimpleDateFormat objSDF = new SimpleDateFormat("dd-mm-yyyy"); 
         LocalDate ld = LocalDate.now();
-
         System.out.print(ld.getDayOfMonth() + "/");
         System.out.print(ld.getMonthValue() + "/");
         System.out.print(ld.getYear());
         // print: 26/8/2020 [ok]
-        
+        LocalDate fecha;
         fecha = LocalDate.now();
-        
-        
         JOptionPane.showMessageDialog(null, fecha.toString(), "LocalDate", JOptionPane.PLAIN_MESSAGE);
         
     }
@@ -33,7 +29,6 @@ public class Test {
     public static void main(String[] args) {
         // prueba1(); -- no static!
         //pruebaObjetoTarea();
-        
     }
     
     private static void pruebaObjetoTarea() {
@@ -48,7 +43,5 @@ public class Test {
         
         JOptionPane.showMessageDialog(null, unaTarea.toString(), "Información de la tarea creada", JOptionPane.PLAIN_MESSAGE);
     }
-    
-    
     
 }
